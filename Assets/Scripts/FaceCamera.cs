07b1a5d17bca92fa76cabad9cad91090d28d298e
miladramhormozi <6170302+miladramhormozi@users.noreply.github.com>
2023-04-11 11:44:18 +0200

    fix camera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    Vector3 directionToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionToPlayer = Camera.main.transform.position - transform.position;
+        transform.forward = directionToPlayer
+        
+    }
+}
